05a917d7820311672e563b5d83ca3ee16a2b7893
MakarCodes <jarek.strupek@wp.pl>
2021-03-30 12:32:08 +0200

    Add reusable button component

---
+++ src/components/Resuable/Button/Button.tsx
@@ -0,0 +1,23 @@
+import classes from './Button.module.scss';
+
+interface IProps {
+  text: string;
+  bgColor: string;
+  testID: string;
+  action: () => void;
+}
+
+const Button: React.FC<IProps> = ({ text, bgColor, testID, action }) => (
+  <button
+    onClick={() => {
+      action();
+    }}
+    className={classes.Btn}
+    style={{ backgroundColor: `${bgColor}` }}
+    data-testid={testID}
+  >
+    {text}
+  </button>
+);
+
+export default Button;
