ace901a369ba47c542bcc1aeb32042cab8653ed8
MakarCodes <jarek.strupek@wp.pl>
2021-03-30 17:42:42 +0200

    Prepare for deploy and make description in readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
+import 'normalize.css';
 import './index.css';
+
 import App from './App';
 import RecipesContextProvider from './context/recipesContext';
 
